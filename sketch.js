34f7cbff1946c2f3dd5cd94b174e52e3d593a0a0
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-07-14 23:34:22 +0530

    test for cloud.js object

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
-var canvasWidth = window.innerWidth;
+var canvasWidth = 0.9 * (window.innerWidth);
 var canvasHeight = 600;
+
+/* GLOBALS */
+var scrollSpeed = 1;
+
+/* OBJECTS */
+var cloud;
 
 function setup() {
 	createCanvas(canvasWidth, canvasHeight);
+	cloud = new Cloud(canvasWidth/2, canvasHeight/2, 5, 1);
 }
 
 function draw() {
-	
+	background("#70EBFF");
+	cloud.draw();
+	cloud.update();
 }
