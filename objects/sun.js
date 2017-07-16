b88ec096831167a7bf47ac5c627fa964ea51bbd6
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-07-16 21:29:56 +0530

    made the sun.draw method:

--- objects/sun.js
+++ objects/sun.js
@@ -9,16 +9,50 @@ var Sun = function(x, y, size) {
 	this.rayCol = color(255, 132, 0);
 };
 Sun.prototype.draw = function() {
+	var numRays = 12;
+	var bodySize = 30;
+
 	noStroke();
+
 	push();
 		translate(this.x, this.y);
 		scale(this.scale);
 		rotate(frameCount);
+
+		//rays shadow
+		push();
+			rotate(SHADOW_OFF);
+			fill(SHADOW);
+			for (var i = 0; i < numRays; i++) {
+				triangle(0, -25, 20, -5, -20, -5);
+				rotate(360 / numRays);
+			}
+		pop();
+
+		//rays
+		push();
+			fill(this.rayCol);
+			for (var i = 0; i < numRays; i++) {
+				triangle(0, -25, 20, -5, -20, -5);
+				rotate(360 / numRays);
+			}
+		pop();
+
+		//sun body
+		rotate(-frameCount); //prevent sun body rotation
+		fill(SHADOW);
+		ellipse(0 + SHADOW_OFF - 1, 0 + SHADOW_OFF - 1, bodySize, bodySize);
 		fill(this.bodyCol);
-		ellipse(0, 0, this.size, this.size);
+		ellipse(0, 0, bodySize, bodySize); 
+
+		/*
+		//debugger:
+		stroke(255, 0, 0);
+		point(5, 0);
+		*/
 	pop();
 };
 Sun.prototype.update = function() {
-	//oscillate around a scale of 1
-	this.scale = this.amp * sin(this.freq * frameCount) + 1;
+	//oscillate around the size
+	this.scale = this.amp * sin(this.freq * frameCount) + this.size;
 };
