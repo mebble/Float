15e268dcaacfbdac4d9f7a460f37d3567646d52a
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-07-17 12:57:13 +0530

    added balloon.js script in index.html and created balloon instance in sketch.js

--- sketch.js
+++ sketch.js
@@ -10,19 +10,25 @@ const SHADOW_OFF = 2;
 /* OBJECTS */
 var cloud;
 var sun;
+var balloon;
 
 function setup() {
 	createCanvas(canvasWidth, canvasHeight);
 	angleMode(DEGREES);
 
-	cloud = new Cloud(canvasWidth/2, canvasHeight/2, 5, 1);
-	sun = new Sun(canvasWidth/2, canvasHeight/2, 1.5);
+	//cloud = new Cloud(canvasWidth/2, canvasHeight/2, 5, 1);
+	//sun = new Sun(canvasWidth/2, canvasHeight/2, 1.5);
+	balloon = new Balloon({
+		color: {
+			pilot: "#007EFF",
+			balloon: "#ff0000",
+			rope: "#ffcc00",
+			basket: "#BA6C07"
+		}
+	});
 }
 
 function draw() {
 	background("#70EBFF");
-	sun.draw();
-	sun.update();
-	cloud.draw();
-	cloud.update();
+	balloon.draw();
 }
