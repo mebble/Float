a5f77baf8f6348fff163500783cff93d4ebc7e4c
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-07-18 23:53:05 +0530

    added keyPressed and keyReleased to events.js

--- mechanics/events.js
+++ mechanics/events.js
@@ -1,7 +1,12 @@
-function keyPressed() {
+/* KEYBOARD */
+var keys = [];
 
+function keyPressed() {
+	keys[keyCode] = true;
+	return false; //prevent default browser behaviour
 }
 
 function keyReleased() {
-	
+	keys[keyCode] = false;
+	return false; //prevent default browser behaviour
 }
