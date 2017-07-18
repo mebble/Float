1a63d97813af3a9ac52fd0b80e3e4c10a0a3adb2
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-07-18 23:54:49 +0530

    created update for balloon.js. x and y are updated to velX and velY, which are set to a constant value. No acceleration included

--- objects/balloon.js
+++ objects/balloon.js
@@ -1,6 +1,8 @@
 var Balloon = function(props) {
 	this.x = props.x || canvasWidth/2;
 	this.y = props.y || canvasHeight/2;
+	this.velX = this.velY = 0;
+	this.speed = 2;
 	this.size = props.size || 120;
 	this.color = props.color || {
 		pilot: "#000",
@@ -22,15 +24,15 @@ Balloon.prototype.draw = function() {
 	Hard code the size of balloon and use
 	scale to resize it for different instances
 	*/
-	noStroke();
 
 	//ropes
-	strokeWeight(5);
+	strokeWeight(4);
 	stroke("#A84B48");
 	for (var i = -1; i <= 1; i++) {
 		line(this.x + i * (basketW/3), this.y, this.x + i * (basketW/3), basketY);
 	}
 
+	noStroke();
 	//balloon
 	fill(this.color.balloon);
 	ellipse(this.x, this.y, this.size, this.size);
@@ -38,8 +40,26 @@ Balloon.prototype.draw = function() {
 	//basket
 	rectMode(CENTER);
 	fill(this.color.basket);
-	rect(basketX, basketY, basketW, basketH);
+	rect(basketX, basketY, basketW, basketH, 3);
 };
 Balloon.prototype.update = function() {
+	//main movement update
+	this.x += this.velX;
+	this.y += this.velY;
 
+	if (keys[UP_ARROW]) {
+		this.velY = -this.speed;
+	} else if (keys[DOWN_ARROW]) {
+		this.velY = this.speed;
+	} else {
+		this.velY = 0;
+	}
+
+	if (keys[RIGHT_ARROW]) {
+		this.velX = this.speed;
+	} else if (keys[LEFT_ARROW]) {
+		this.velX = -this.speed;
+	} else {
+		this.velX = 0;
+	}
 };
