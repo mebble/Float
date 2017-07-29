f6cc9dd6cac831fab9f92fc65721dc0f0f7d97c2
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-07-29 16:22:19 +0530

    included ground.js and mountain.js

--- sketch.js
+++ sketch.js
@@ -8,12 +8,18 @@ const SHADOW = "rgba(130, 130, 130, 10)";
 const SHADOW_OFF = 2;
 
 /* OBJECTS */
+var mountain;
+var ground;
 
 function setup() {
 	createCanvas(canvasWidth, canvasHeight);
 	angleMode(DEGREES);
+	ground = new Ground(5/6 * canvasHeight, '#6DBE40');
+	mountain = new Mountain(canvasWidth/2, ground.y, 300, 270);
 }
 
 function draw() {
 	background("#4cb5f5");
+	mountain.draw();
+	ground.draw();
 }
