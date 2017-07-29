d53e1fd20994ef638f5184a153aa27630f9f78b8
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-07-29 16:21:47 +0530

    ground.js is now a constuctor instead of a hard-coded object

--- objects/ground.js
+++ objects/ground.js
@@ -1,4 +1,9 @@
-var ground = {
-	y: (6/7) * canvasHeight,
-	color: "#ffffff"
+var Ground = function(y, color) {
+	this.y = y;
+	this.color = color;
+};
+Ground.prototype.draw = function() {
+	noStroke();
+	fill(this.color);
+	rect(-10, this.y, canvasWidth + 10, canvasHeight - this.y + 10);
 };
