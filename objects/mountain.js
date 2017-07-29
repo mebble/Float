b6d9dd7c19ee89a70e7b9f14985b277bef600430
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-07-29 16:19:34 +0530

    mountain.js takes x, y, width, height args instead of a params object. Corners are made based on these values

--- objects/mountain.js
+++ objects/mountain.js
@@ -1,25 +1,23 @@
-var Mountain = function(params) {
-	/**
-	params = {
-		peak: [],
-		left: [],
-		middle: [],
-		right: []
-	}
-	*/
-	this.params = params;
+var Mountain = function(peakX, baseY, width, height) {
+	this.corners = {
+		peak: [peakX, baseY - height],
+		left: [peakX - width/2, baseY],
+		middle: [peakX, baseY],
+		right: [peakX + width/2, baseY]
+	};
 	this.leftShade = color(232, 167, 130);
 	this.rightShade = color(194, 149, 128);
 };
 Mountain.prototype.draw = function() {
+	noStroke();
 	/* mountain */
 	beginShape(TRIANGLE_STRIP);
 	fill(this.leftShade);
-	vertex(...this.params.left);
-	vertex(...this.params.peak);
-	vertex(...this.params.middle);
+	vertex(...this.corners.left);
+	vertex(...this.corners.peak);
+	vertex(...this.corners.middle);
 	fill(this.rightShade);
-	vertex(...this.params.right);
+	vertex(...this.corners.right);
 	endShape();
 
 	/* snowcap */
