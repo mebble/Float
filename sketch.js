8f24f46886a67762764e2af8430fd86d3076f2b0
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-08-02 23:01:41 +0530

    test for pine.js

--- sketch.js
+++ sketch.js
@@ -9,22 +9,22 @@ const SHADOW = "rgba(130, 130, 130, 10)";
 const SHADOW_OFF = 2;
 
 /* OBJECTS */
-var mountain;
 var ground;
+var pine;
 
 function setup() {
 	canvas = createCanvas(canvasWidth, canvasHeight);
 	centerCanvas();
-
 	angleMode(DEGREES);
-	ground = new Ground(5/6 * canvasHeight, '#6DBE40');
-	mountain = new Mountain(canvasWidth/2, ground.y, 400, 270);
+
+	ground = new Ground(5/6 * canvasHeight, '#679D48');
+	pine = new Pine(canvasWidth/2, canvasHeight/2, [80, 154, 114]);
 }
 
 function draw() {
 	background("#4cb5f5");
-	mountain.draw();
-	mountain.update();
+	pine.draw();
+	pine.update();
 	ground.draw();
 }
 
