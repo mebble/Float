3369f004b46f8d3e24385ea3350c7a1744ad3bfe
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-08-18 17:50:48 +0530

    x, extraBleed in ground.js, test for ground

--- objects/ground.js
+++ objects/ground.js
@@ -1,9 +1,14 @@
-var Ground = function(y, color) {
+var Ground = function(x, y, color) {
+	this.x = x;
 	this.y = y;
+	this.extraBleed = 10;
 	this.color = color;
 };
 Ground.prototype.draw = function() {
 	noStroke();
 	fill(this.color);
-	rect(-10, this.y, canvasWidth + 10, canvasHeight - this.y + 10);
+	rect(this.x - this.extraBleed,
+		 this.y,
+		 canvasWidth + (2 * this.extraBleed),
+		 canvasHeight - this.y + this.extraBleed);
 };
--- sketch.js
+++ sketch.js
@@ -10,21 +10,17 @@ const SHADOW_OFF = 2;
 
 /* OBJECTS */
 var ground;
-var wind;
 
 function setup() {
 	canvas = createCanvas(canvasWidth, canvasHeight);
 	centerCanvas();
 	angleMode(DEGREES);
 
-	ground = new Ground(5/6 * canvasHeight, "#1CE29F");
-	wind = new Wind(canvasWidth/2, canvasHeight/2, 0.15);
+	ground = new Ground(0, 5/6 * canvasHeight, "#1CE29F");
 }
 
 function draw() {
 	background("#4cb5f5");
-	wind.draw();
-	wind.update();
 	ground.draw();
 }
 
