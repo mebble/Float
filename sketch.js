f2e3a69c7a6086511a54826a0e942c5107e41520
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-08-18 18:36:00 +0530

    test for foreTreeQ

--- sketch.js
+++ sketch.js
@@ -9,23 +9,26 @@ const SHADOW = "rgba(130, 130, 130, 10)";
 const SHADOW_OFF = 2;
 
 /* OBJECTS */
-var ground;
-var wind;
+
 
 function setup() {
 	canvas = createCanvas(canvasWidth, canvasHeight);
 	centerCanvas();
 	angleMode(DEGREES);
 
-	ground = new Ground(5/6 * canvasHeight, "#1CE29F");
-	wind = new Wind(canvasWidth/2, canvasHeight/2, 0.15);
+	initQueue(foreTreeQ, {
+		initNum: 25,
+		xStart: 0,
+		xStep: 20,
+		classType: Pine
+	});
 }
 
 function draw() {
 	background("#4cb5f5");
-	wind.draw();
-	wind.update();
-	ground.draw();
+	foreTreeQ.forEach(function(elem) {
+		elem.draw();
+	});
 }
 
 function centerCanvas() {
