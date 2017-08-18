1c41f75aa48f41135f0eed2355ea6d443e9b8a2c
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2017-08-18 19:00:30 +0530

    hard coded args in classType call

--- environment/mechanics.js
+++ environment/mechanics.js
@@ -3,6 +3,6 @@ function initQueue(queue, params) {
 	var x;
 	for (var i = 0; i < p.initNum; i++) {
 		x = p.xStart + i * p.xStep;
-		queue.push(new p.classType());
+		queue.push(new p.classType(x, random(0.7 * canvasHeight, 0.8 * canvasHeight), "#D27C2C", scrollSpeed));
 	}
 }
