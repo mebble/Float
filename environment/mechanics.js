c52b109869d85f812a3f7c8960de4bd519ffcbe0
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2017-08-21 21:30:48 +0530

    update docs

--- environment/mechanics.js
+++ environment/mechanics.js
@@ -1,19 +1,17 @@
 function initQueue(params, modifyConfig) {
 	/**
-	 * initQueue(params, config, modifyConfig);
+	 * initQueue:
 	 * 'params' contains properties of the queue.
-	 * 'config' contains properties of each element of the queue.
+	 * 'params.config' contains properties of each element of the queue.
 	 * 'modifyConfig' modifies the config object for the next queue element.
-	 *
-	 * The config object arg into initQueue is the inital config object,
-	 * which holds the properties of the first initialized elem of the queue.
 	 */
 
 	/**
-	 * modifyConfig(config, params);
-	 * Modifies the config object based on the properties
+	 * modifyConfig:
+	 * Modifies the params.config object based on the properties
 	 * of the params object.
 	 */
+	 
 	for (var i = 0; i < params.initNum; i++) {
 		params.queue.push(new params.classType(params.config));
 		modifyConfig(params);
